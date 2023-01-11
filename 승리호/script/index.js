6ea0d4af501f13b0635901f26b5eabf2e4a51b79
easy0-0 <122326433+easy0-0@users.noreply.github.com>
2023-01-11 19:32:35 +0900

    Update index.js

--- 승리호/script/index.js
+++ 승리호/script/index.js
@@ -1,15 +1,15 @@
-$('#play01').on('click',function(){
-    $('#v01').get(0).play();
-    $(this).hide();
+$('#v01').on('click',function(){
+    $(this).get(0).play();
+    $('#play01').hide();
 });
 $('#v01').on('click',function(){
     $(this).get(0).pause();
     $('#play01').show();
 });
 
-$('#play02').on('click',function(){
-    $('#v02').get(0).play();
-    $(this).hide();
+$('#v02').on('click',function(){
+    $(this).get(0).play();
+    $('#play02').hide();
 });
 $('#v02').on('click',function(){
     $(this).get(0).pause();
